89dfd5238926276e0ca57c7509918ad0fb53f6cc
Gen4ralz <acetyl-coa@windowslive.com>
2022-08-30 23:05:49 +0700

    List-Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">Dosimple</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.imageHome} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>
+                  <strong>{product.price} ฿</strong>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Momo set',
+      slug: 'momo-set',
+      category: 'sets',
+      imageHome: '/images/momo/momo-pink1.jpg',
+      price: 490,
+      brand: 'Gamclothes',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality clothes',
+      colors: [
+        { name: 'Pink', class: 'bg-pink', countInStock: 10 },
+        { name: 'Brown', class: 'bg-brown', countInStock: 8 },
+        { name: 'Cream', class: 'bg-cream', countInStock: 10 },
+      ],
+    },
+    {
+      name: 'Maxi Pop dress',
+      slug: 'maxi-pop-dress',
+      category: 'dress',
+      imageHome: '/images/maxipopdress/maxipop-green1.jpg',
+      price: 550,
+      brand: 'Gamclothes',
+      rating: 4.8,
+      numReviews: 7,
+      description: 'High quality clothes',
+      colors: [
+        { name: 'Green', class: 'bg-green', countInStock: 10 },
+        { name: 'Violet', class: 'bg-violet', countInStock: 8 },
+        { name: 'Yellow', class: 'bg-yellow', countInStock: 10 },
+      ],
+    },
+    {
+      name: 'Lanie Set',
+      slug: 'lanie-set',
+      category: 'sets',
+      imageHome: '/images/lanie/lanie-white1.jpg',
+      price: 590,
+      brand: 'Gamclothes',
+      rating: 4.8,
+      numReviews: 7,
+      description: 'High quality clothes',
+      colors: [
+        { name: 'White', class: 'bg-white', countInStock: 10 },
+        { name: 'Blue', class: 'bg-blue', countInStock: 8 },
+        { name: 'Violet', class: 'bg-violet', countInStock: 10 },
+      ],
+    },
+  ],
+};
+
+export default data;
