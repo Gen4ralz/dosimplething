fae34bb0423ac40d20580d76873df13a180109aa
Gen4ralz <acetyl-coa@windowslive.com>
2022-09-01 00:50:15 +0700

    Add-Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,40 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">Dosimple</Link>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Dosimple</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <Navbar bg="light" fixed="bottom">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Alright reserve</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </footer>
       </div>
     </BrowserRouter>
   );
