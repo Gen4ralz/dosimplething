f429d93e4b4c48388ae5b9d9cdbea334dab1727d
Gen4ralz <acetyl-coa@windowslive.com>
2022-09-13 21:22:26 +0700

    Create-User-Profile-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceorderScreen from './screens/PlaceorderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -29,6 +30,7 @@ function App() {
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
     localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin';
   };
 
   return (
@@ -94,6 +96,7 @@ function App() {
               <Route path="/placeorder" element={<PlaceorderScreen />} />
               <Route path="/order/:id" element={<OrderScreen />} />
               <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
             </Routes>
           </Container>
         </main>
